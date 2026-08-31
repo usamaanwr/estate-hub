17d602e9ce4cfeac946f439770dfaa016e81ec63
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-08-31 22:24:10 +0500

    feat: add authorizeRole middleware for role-based access control

---
+++ server/src/middleware/authorizeRoles.middleware.js
@@ -0,0 +1,10 @@
+import {ApiError} from "../../utils/ApiError.js"
+
+export const authorizeRole = (...allowedRoles)=>{
+    return(req , res , next)=>{
+        if (!allowedRoles.includes(req.user.role)) {
+            throw new ApiError(404 ,"you are not authoried tp perform this action");
+            next();
+        }
+    }
+}
