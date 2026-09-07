e3cf455e94f6b62aed948de62d1e67dde5becf6a
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2026-09-08 00:01:49 +0500

    fix: correct authorization error message and ensure next middleware is called

--- server/src/middleware/authorizeRoles.middleware.js
+++ server/src/middleware/authorizeRoles.middleware.js
@@ -3,8 +3,8 @@ import {ApiError} from "../../utils/ApiError.js"
 export const authorizeRole = (...allowedRoles)=>{
     return(req , res , next)=>{
         if (!allowedRoles.includes(req.user.role)) {
-            throw new ApiError(404 ,"you are not authoried tp perform this action");
-            next();
+            throw new ApiError(404 ,"you are not authoried to perform this action");
         }
+        next();
     }
 }
